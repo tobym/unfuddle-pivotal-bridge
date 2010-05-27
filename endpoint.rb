40959928c3ea9be35374dd94135c754ef915a8c8
Toby Matejovsky <toby.matejovsky@gmail.com>
2010-05-27 17:57:33 +0000

    Switch endpoint to rackup style, for easy daemonization

--- endpoint.rb
+++ endpoint.rb
@@ -1,10 +1,12 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'unfuddle_pivotal_bridge'
 
-post '/unfuddle-pivotal-bridge' do
-  raw = request.body.string
-  bridge = UnfuddlePivotalBridge.new
-  bridge.parse_unfuddle_changeset(raw)
-  bridge.add_note
-  "Success"
+class UnfuddlePostcommitEndpoint < Sinatra::Base
+  post '/unfuddle-pivotal-bridge' do
+    raw = request.body.read
+    bridge = UnfuddlePivotalBridge.new
+    bridge.parse_unfuddle_changeset(raw)
+    bridge.add_note
+    "Success"
+  end
 end
